cbcecf89f2165d16ae887f323c74e323fe575c29
dnatividade <dvnatividade@gmail.com>
2023-10-06 10:39:34 -0300

    Up

---
+++ 01 Simple Projects/013 Simple flooding to find a car/src/veins/modules/application/traci/CA.cc
@@ -0,0 +1,40 @@
+//
+// Copyright (C) 2016 David Eckhoff <david.eckhoff@fau.de>
+//
+// Documentation for these modules is at http://veins.car2x.org/
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//App: @dnat
+
+
+#include <veins/modules/application/traci/CA.h>
+
+using namespace veins;
+
+Define_Module(veins::CA);
+
+
+void CA::onWSM(BaseFrame1609_4* frame) {
+
+    //if (Message* wsm = check_and_cast<Message*>(frame)) {
+    //}
+}
+
+void CA::finish(void) {
+}
+
---
+++ 01 Simple Projects/013 Simple flooding to find a car/src/veins/modules/application/traci/defines.h
@@ -0,0 +1,19 @@
+/*
+ * @brief
+ * Include file to use in "<this> Project"
+ * This file contain all necessary defines to use in Cars,RSU and CA
+ */
+//@dnat
+
+#define RSU1 11 //not used in this project
+#define CA1  16 //not used in this project
+#define CAR1 400 //source car
+#define CAR2 28  //destination car
+#define TIME 200 //time to start the search
+
+// Type of MsgFind
+#define FIND_REQUEST 0
+#define FIND_REPLAY  1
+#define FIND_CANCEL  2
+
+
